6726a6aa9c9d8eae2ce1300e1dc91dcbe6d8c885
trashpirate <nadinaoates@gmail.com>
2024-02-28 03:25:28 -0600

    adjust border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     <main className="flex h-screen flex-col justify-items-stretch bg-black bg-scroll p-4 text-black bg-blend-darken">
       <div className="m-auto w-full flex-col items-center xl:w-5/6">
         <Navbar></Navbar>
+        {/* <h1 className="my-8 h-10 text-center text-xl font-bold uppercase drop-shadow-text xs:text-2xl md:text-3xl"></h1> */}
         <div className="mt-8 grid w-full grid-cols-1 justify-between justify-items-stretch gap-4 md:grid-cols-[25%_30%_40%]">
           <div className="flex w-full flex-col justify-stretch">
             <CollectionInfo></CollectionInfo>
