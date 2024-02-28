8ece003fa811b6b35f5ee6a69a1bd7c8a8e143bc
trashpirate <nadinaoates@gmail.com>
2024-02-28 02:25:43 -0600

    adjusting margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import About from "@/components/about/about";
 export default function Home() {
   return (
     <main className="flex h-screen flex-col justify-items-stretch bg-black bg-scroll p-4 text-black bg-blend-darken">
-      <div className="mx-auto h-full w-full flex-col items-center justify-between xl:w-5/6">
+      <div className="m-auto h-full w-full flex-col items-center justify-between xl:w-5/6">
         <Navbar></Navbar>
         {/* <h1 className="my-8 h-10 text-center text-xl font-bold uppercase drop-shadow-text xs:text-2xl md:text-3xl"></h1> */}
         <div className="mt-4 grid w-full grid-cols-1 justify-between justify-items-stretch gap-4 md:grid-cols-[25%_30%_40%]">
