43266fc432edb3f5ee8a74a1a3dfb518dde8e9a4
trashpirate <nadinaoates@gmail.com>
2024-02-28 02:33:36 -0600

    adjusting margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,15 @@ import About from "@/components/about/about";
 export default function Home() {
   return (
     <main className="flex h-screen flex-col justify-items-stretch bg-black bg-scroll p-4 text-black bg-blend-darken">
-      <div className="m-auto h-fit w-full flex-col items-center justify-between xl:w-5/6">
+      <div className="m-auto w-full flex-col items-center xl:w-5/6">
         <Navbar></Navbar>
-        {/* <h1 className="my-8 h-10 text-center text-xl font-bold uppercase drop-shadow-text xs:text-2xl md:text-3xl"></h1> */}
-        <div className="mt-4 grid w-full grid-cols-1 justify-between justify-items-stretch gap-4 md:grid-cols-[25%_30%_40%]">
-          <div className="flex h-full w-full flex-col justify-stretch">
+        <div className="mt-8 grid w-full grid-cols-1 justify-between justify-items-stretch gap-4 md:grid-cols-[25%_30%_40%]">
+          <div className="flex w-full flex-col justify-stretch">
             <CollectionInfo></CollectionInfo>
             <AccountInfo></AccountInfo>
           </div>
           <Minter></Minter>
-          <div className="flex h-full w-full flex-col justify-stretch">
+          <div className="flex w-full flex-col justify-stretch">
             <Nfts></Nfts>
             <About></About>
           </div>
